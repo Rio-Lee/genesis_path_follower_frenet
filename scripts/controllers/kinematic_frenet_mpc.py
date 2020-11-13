a182b0f94aa2cf6613ed48031eb521efc89e601c
Rio <ssmjibong@gmail.com>
2020-11-12 17:20:40 -0800

    First commit

---
+++ scripts/controllers/kinematic_frenet_mpc.py
@@ -0,0 +1,247 @@
+# This is a modified version of kinematic_mpc.py from genesis_path_follower GitHub repo.
+
+import time
+import casadi
+import numpy as np
+from controller import Controller
+
+class KinMPCPathFollower(Controller):
+	##
+	def __init__(self, 
+		         N          = 10,     # timesteps in MPC Horizon
+		         DT_MODEL	= 0.2,    # discretization time between timesteps (s)
+		         L_F        = 1.5213, # distance from CoG to front axle (m)
+		         L_R        = 1.4987, # distance from CoG to rear axle (m)
+				 V_SET		= 45./2.237,	# velocity set by a driver (m/s)
+				 AX_MAX     =  5.0,		
+		         AX_MIN     = -10.0,   # min/max acceleration constraint (m/s^2) 
+				 AY_MAX     =  4.0,		
+				 AY_MIN     = -4.0,  	
+		         DF_MAX     =  30*np.pi/180,
+		         DF_MIN     = -30*np.pi/180,   # min/max front steer angle constraint (rad)
+		         AX_DOT_MAX  =  3.,
+		         AX_DOT_MIN  = -3.,   # min/max jerk constraint (m/s^3)
+				 AY_DOT_MAX  =  5.,
+		         AY_DOT_MIN  = -5.,   # min/max jerk constraint (m/s^3)
+		         DF_DOT_MAX =  30*np.pi/180,
+		         DF_DOT_MIN = -30*np.pi/180,   # min/max front steer angle rate constraint (rad/s)
+		         EY_MAX		=  0.8,
+				 EY_MIN		= -0.8,
+				 EPSI_MAX	=  10*np.pi/180,
+				 EPSI_MIN	= -10*np.pi/180,
+				 Q = [0., 100., 500., 1., 0.],  # s, ey, epsi, v, ay
+				 R = [.01, .001]):  # ax, df
+
+		for key in list(locals()):
+			if key == 'self':
+				pass
+			elif key == 'Q':
+				self.Q = casadi.diag(Q)
+			elif key == 'R':
+				self.R = casadi.diag(R)
+			else:
+				setattr(self, '%s' % key, locals()[key])
+
+		self.opti = casadi.Opti()
+
+		''' 
+		(1) Parameters
+		'''		
+		##
+		self.u_prev  = self.opti.parameter(2) # previous input: [u_{acc, -1}, u_{df, -1}]
+		self.z_curr  = self.opti.parameter(5) # current state:  [s0, ey_0, epsi_0, v_0, Not Used]
+
+		# Reference trajectory we would like to follow.
+		# First index corresponds to our desired state at timestep k+1:
+		#   i.e. z_ref[0,:] = z_{desired, 1}.
+		# Second index selects the state element from [x_k, y_k, psi_k, v_k].
+		# Note that, in this controller, v_k set as a constant value, v_ref, is used for the entire horizon N.
+
+		self.z_ref = self.opti.parameter(1, 5) # [Not Used, Not Used, Not Used, v_ref, Not Used]
+
+		## Reference curvature we would like to follow.
+		self.curv_ref = self.opti.parameter(self.N)
+
+		'''
+		(2) Decision Variables
+		'''
+		## First index is the timestep k, i.e. self.z_dv[0,:] is z_0.		
+		## It has self.N+1 timesteps since we go from z_0, ..., z_self.N.
+		## Second index is the state element, as detailed below.
+		self.z_dv = self.opti.variable(self.N+1, 5) # s, ey, epsi, v, ay
+	
+		self.s_dv   = self.z_dv[:, 0]  
+		self.ey_dv   = self.z_dv[:, 1]  
+		self.epsi_dv = self.z_dv[:, 2]  
+		self.v_dv   = self.z_dv[:, 3]  
+		self.ay_dv   = self.z_dv[:, 4]
+		
+		## Control inputs used to achieve self.z_dv according to dynamics.
+		## First index is the timestep k, i.e. self.u_dv[0,:] is u_0.
+		## Second index is the input element as detailed below.
+		self.u_dv = self.opti.variable(self.N, 2)
+
+		self.acc_dv = self.u_dv[:,0]
+		self.df_dv  = self.u_dv[:,1]
+
+		# Slack variables used to relax input rate constraints.
+		# Matches self.u_dv in structure but timesteps range from -1, ..., N-1.
+
+		self.sl_dv  = self.opti.variable(self.N , 1)
+		
+		self.sl_ay_dv = self.sl_dv[:,0]
+		
+		'''
+		(3) Problem Setup: Constraints, Cost, Initial Solve
+		'''
+
+		self._add_constraints()
+
+		self._add_cost()	
+
+		self._update_initial_condition(0., 0., 0., 1., 0.)
+		
+		self._update_reference(self.N*[0.], [0., 0., 0., 20., 0.])
+		
+		self._update_previous_input(0., 0.)
+		
+		# Ipopt with custom options: https://web.casadi.org/docs/ -> see sec 9.1 on Opti stack.
+		p_opts = {'expand': True}
+		s_opts = {'max_cpu_time': 0.2, 'print_level': 0} 
+		self.opti.solver('ipopt', p_opts, s_opts)
+
+		sol = self.solve()
+
+	def _add_constraints(self):
+		## State Bound Constraints
+		self.opti.subject_to( self.opti.bounded(self.EY_MIN, self.ey_dv, self.EY_MAX) )
+		self.opti.subject_to( self.opti.bounded(self.EPSI_MIN, self.epsi_dv, self.EPSI_MAX) )
+		for i in range(self.N):
+			self.opti.subject_to( self.opti.bounded(self.AY_MIN - self.sl_ay_dv[i], self.ay_dv, self.AY_MAX + self.sl_ay_dv[i]) )		
+
+		## Initial State Constraint
+		self.opti.subject_to( self.s_dv[0]   == self.z_curr[0] )   
+		self.opti.subject_to( self.ey_dv[0]   == self.z_curr[1] )  
+		self.opti.subject_to( self.epsi_dv[0] == self.z_curr[2] )  
+		self.opti.subject_to( self.v_dv[0]   == self.z_curr[3] )   
+		
+		## State Dynamics Constraints
+		for i in range(self.N):
+			beta = casadi.atan( self.L_R / (self.L_F + self.L_R) * casadi.tan(self.df_dv[i]) )
+			dyawdt = self.v_dv[i] / self.L_R * casadi.sin(beta)
+			dsdt = self.v_dv[i] * casadi.cos(self.epsi_dv[i]+beta) / (1 - self.ey_dv[i] * self.curv_ref[i] )   
+			
+
+			self.opti.subject_to( self.s_dv[i+1] == self.s_dv[i] + self.DT_MODEL * (dsdt) )  
+			self.opti.subject_to( self.ey_dv[i+1] == self.ey_dv[i] + self.DT_MODEL * (self.v_dv[i]) * casadi.sin(self.epsi_dv[i] + beta) ) 
+			self.opti.subject_to( self.epsi_dv[i+1] == self.epsi_dv[i] + self.DT_MODEL * (dyawdt - dsdt * self.curv_ref[i]) )
+			self.opti.subject_to( self.v_dv[i+1] == self.v_dv[i] + self.DT_MODEL * (self.acc_dv[i]) )
+			self.opti.subject_to( self.ay_dv[i] == self.v_dv[i] * dyawdt)
+            
+		## Input Bound Constraints
+		self.opti.subject_to( self.opti.bounded(self.AX_MIN,  self.acc_dv, self.AX_MAX) )
+		self.opti.subject_to( self.opti.bounded(self.DF_MIN, self.df_dv,  self.DF_MAX) )
+
+		# Input Rate Bound Constraints
+		self.opti.subject_to( self.opti.bounded( self.AX_DOT_MIN, 
+			                                     self.acc_dv[0] - self.u_prev[0],
+			                                     self.AX_DOT_MAX) )
+
+		self.opti.subject_to( self.opti.bounded( self.DF_DOT_MIN, 
+			                                     self.df_dv[0] - self.u_prev[1],
+			                                     self.DF_DOT_MAX) )
+
+		for i in range(self.N - 1):
+			self.opti.subject_to( self.opti.bounded( self.AX_DOT_MIN, 
+				                                     self.acc_dv[i+1] - self.acc_dv[i],
+				                                     self.AX_DOT_MAX) )
+			self.opti.subject_to( self.opti.bounded( self.DF_DOT_MIN, 
+				                                     self.df_dv[i+1]  - self.df_dv[i],
+				                                     self.DF_DOT_MAX) )
+		# Other Constraints
+
+		self.opti.subject_to( self.opti.bounded(0, self.sl_ay_dv, 1) )
+		# e.g. things like collision avoidance or lateral acceleration bounds could go here.
+	
+	## Cost function
+	def _add_cost(self):
+		def _quad_form(z, Q):
+			return casadi.mtimes(z, casadi.mtimes(Q, z.T))	
+		
+		cost = 0
+		for i in range(self.N):
+			cost += _quad_form(self.z_dv[i, :]-self.z_ref, self.Q)
+
+		for i in range(self.N - 1):
+			cost += _quad_form(self.u_dv[i+1, :] - self.u_dv[i,:], self.R)
+		
+		cost += casadi.sum1(self.sl_ay_dv)
+		self.opti.minimize( cost )
+
+	def solve(self):
+		st = time.time()
+		try:
+			sol = self.opti.solve()
+			# Optimal solution.
+			u_mpc  = sol.value(self.u_dv)
+			z_mpc  = sol.value(self.z_dv)
+			sl_mpc = sol.value(self.sl_dv)
+			z_ref  = sol.value(self.z_ref)
+			is_opt = True
+		except:
+			# Suboptimal solution (e.g. timed out).
+			u_mpc  = self.opti.debug.value(self.u_dv)
+			z_mpc  = self.opti.debug.value(self.z_dv)
+			sl_mpc = self.opti.debug.value(self.sl_dv)
+			z_ref  = self.opti.debug.value(self.z_ref)
+			is_opt = False
+
+		solve_time = time.time() - st
+		
+		sol_dict = {}
+		sol_dict['u_control']  = u_mpc[0,:]  # control input to apply based on solution
+		sol_dict['optimal']    = is_opt      # whether the solution is optimal or not
+		sol_dict['solve_time'] = solve_time  # how long the solver took in seconds
+		sol_dict['u_mpc']      = u_mpc       # solution inputs (N by 2, see self.u_dv above) 
+		sol_dict['z_mpc']      = z_mpc       # solution states (N+1 by 4, see self.z_dv above)
+		sol_dict['sl_mpc']     = sl_mpc      # solution slack vars (N by 2, see self.sl_dv above)
+		sol_dict['z_ref']      = z_ref       # state reference (N by 4, see self.z_ref above)
+
+		return sol_dict
+
+	def update(self, update_dict):
+		# TODO: 'psi0' should be replaced by 'ay0' later.
+		self._update_initial_condition( *[update_dict[key] for key in ['s', 'e_y', 'e_psi', 'v0', 'psi0']] )	
+		
+		# Calculate the maximum from the upcoming curvatures.
+		curvature_max = max(abs(update_dict['curv_ref']))
+		v_ref = np.sqrt(self.AY_MAX/abs(curvature_max))
+		v_ref = min(self.V_SET, v_ref)
+		self._update_reference( update_dict['curv_ref'], [0, 0, 0, v_ref, 0] )
+
+		self._update_previous_input( *[update_dict[key] for key in ['acc_prev', 'df_prev']] )
+
+		if 'warm_start' in update_dict.keys():
+			# Warm Start used if provided.  Else I believe the problem is solved from scratch with initial values of 0.
+			self.opti.set_initial(self.z_dv,  update_dict['warm_start']['z_ws'])
+			self.opti.set_initial(self.u_dv,  update_dict['warm_start']['u_ws'])
+			self.opti.set_initial(self.sl_dv, update_dict['warm_start']['sl_ws'])
+
+	##
+	def _update_initial_condition(self, s0, ey0, epsi0, vel0, ay0):
+		self.opti.set_value(self.z_curr, [s0, ey0, epsi0, vel0, ay0])
+
+	##
+	def _update_reference(self, curv_ref, z_ref):
+		self.opti.set_value(self.curv_ref, curv_ref)
+		self.opti.set_value(self.z_ref, z_ref)
+
+	def _update_previous_input(self, acc_prev, df_prev):
+		self.opti.set_value(self.u_prev, [acc_prev, df_prev])
+
+if __name__ == '__main__':
+	kmpc = KinMPCPathFollower()
+	sol_dict = kmpc.solve()
+	
+	for key in sol_dict:
+		print(key, sol_dict[key])
---
+++ scripts/mpc_cmd_pub.py
@@ -0,0 +1,182 @@
+#!/usr/bin/env python
+
+# MPC Command Publisher/Controller Module Interface to the Genesis.
+
+import sys
+from threading import Lock
+
+###########################################
+#### ROS
+###########################################
+import rospy
+from genesis_path_follower.msg import state_est
+from genesis_path_follower.msg import mpc_path
+from std_msgs.msg import UInt8 as UInt8Msg
+from std_msgs.msg import Float32 as Float32Msg
+
+###########################################
+#### LOAD ROSPARAMS AND UPDATE SYS.PATH
+###########################################
+if rospy.has_param("mat_waypoints"):
+	mat_fname = rospy.get_param("mat_waypoints")
+else:
+	raise ValueError("No Matfile of waypoints provided!")
+
+if rospy.has_param("track_using_time") and rospy.has_param("target_vel"):
+	track_with_time = rospy.get_param("track_using_time")
+	target_vel = rospy.get_param("target_vel")	
+else:
+	raise ValueError("Invalid rosparam trajectory definition: track_using_time and target_vel")
+
+if rospy.has_param("scripts_dir"):
+	scripts_dir = rospy.get_param("scripts_dir")
+else:
+	raise ValueError("Did not provide the scripts directory!")
+
+if rospy.has_param("lat0") and rospy.has_param("lon0"):
+	lat0 = rospy.get_param("lat0")
+	lon0 = rospy.get_param("lon0")	
+else:
+	raise ValueError("Invalid rosparam global origin provided!")
+
+if rospy.has_param("controller"):
+	controller_choice = rospy.get_param("controller")
+else:
+	raise ValueError("Did not select a controller!")
+
+# Access Python code for MPC/path utils.  Ugly way of doing it, can seek to clean this up in the future.
+sys.path.append(scripts_dir)
+
+###########################################
+#### MPC Controller Module with Cost Function Weights.
+#### Reference GPS Trajectory Module.
+###########################################
+# TODO: We can make some of these things rosparams in the future, like N and DT.
+if controller_choice == 'kinematic_mpc':
+	from controllers.kinematic_mpc import KinMPCPathFollower as MPC
+	MPC_PARAMS = {'N' : 10, 'DT' : 0.2, 'Q' : [1., 1., 10., 0.0], 'R' : [10., 100.]}
+elif controller_choice == 'kinematic_frenet_mpc':
+	pass
+else:
+	raise ValueError("Invalid controller selection: %s" % controller_choice)
+
+from gps_utils import ref_gps_traj as RGT
+
+###########################################
+#### MPC Command Publisher Class
+###########################################
+class MPCCommandPublisher():
+
+	def __init__(self):	
+		# Node and Publisher/Subscriber Setup.
+		rospy.init_node("dbw_mpc_path_follower")
+		
+		# Acceleration/Steering Command (for drive-by-wire) + MPC solution publisher (for viz/analysis).
+		self.acc_prev   = 0. # last published acceleration
+		self.steer_prev = 0. # last published steering angle
+		self.acc_pub   = rospy.Publisher("/control/accel", Float32Msg, queue_size=2)      
+		self.steer_pub = rospy.Publisher("/control/steer_angle", Float32Msg, queue_size=2) 
+		self.mpc_path_pub = rospy.Publisher("mpc_path", mpc_path, queue_size=2)            
+
+		# State Estimation Subscriber.
+		self.current_state = {'t': -1., 'x0': 0., 'y0': 0., 'psi0': 0., 'v0': 0}
+		self.state_lock = Lock()
+		self.sub_state  = rospy.Subscriber("state_est", state_est, self.state_est_callback, queue_size=2)
+
+		# TODO: update description and make yaw/psi consistent.
+		self.mpc = MPC(**MPC_PARAMS)
+		self.ref_traj = RGT.GPSRefTrajectory(mat_filename=mat_fname, LAT0=lat0, LON0=lon0, traj_horizon=self.mpc.N, traj_dt=self.mpc.DT)
+		
+		# One time drive-by-wire enable message published.  We assume the driver will disable drive-by-wire
+		# using brake/steering overrides, although it could be done by this class too.
+		acc_enable_pub   = rospy.Publisher("/control/enable_accel", UInt8Msg, queue_size=2, latch=True)
+		steer_enable_pub = rospy.Publisher("/control/enable_spas",  UInt8Msg, queue_size=2, latch=True)
+		acc_enable_pub.publish(UInt8Msg(1))
+		steer_enable_pub.publish(UInt8Msg(1))
+		
+		self.pub_loop()
+
+	def state_est_callback(self, msg):
+		with self.state_lock:
+			self.current_state['t']    = msg.header.stamp.secs + 1e-9 * msg.header.stamp.nsecs
+			self.current_state['x0']   = msg.x
+			self.current_state['y0']   = msg.y
+			self.current_state['psi0'] = msg.psi
+			self.current_state['v0']   = msg.v			
+			
+	def pub_loop(self):
+		loop_rate = rospy.Rate(50.0)
+		update_dict = {} # contains parameter/warm start information needed for the MPC module.
+
+		while not rospy.is_shutdown():
+			with self.state_lock:
+				state = self.current_state.copy()				
+			
+			if state['t'] < 0:
+				# The state has not been received so don't start publishing yet.
+				loop_rate.sleep()
+				continue
+
+			if track_with_time:
+				# Trajectory tracking with varying speed profile.
+				waypoint_dict = self.ref_traj.get_waypoints(state['x0'], state['y0'], state['psi0'], v_target=None)
+			else:
+				# Trajectory tracking with a fixed speed profile.
+				waypoint_dict = self.ref_traj.get_waypoints(state['x0'], state['y0'], state['psi0'], v_target=target_vel)	
+
+			if waypoint_dict['stop']:
+				self.acc_pub.publish(Float32Msg(-1.0))
+				self.steer_pub.publish(Float32Msg(0.0))
+			else:
+				update_dict.update(state)
+				update_dict.update(waypoint_dict)
+				update_dict['acc_prev'] = self.acc_prev
+				update_dict['df_prev']  = self.steer_prev			
+
+				self.mpc.update(update_dict)
+				sol_dict = self.mpc.solve()
+
+				if sol_dict['optimal']:
+					self.acc_pub.publish(Float32Msg(sol_dict['u_control'][0]))
+					self.steer_pub.publish(Float32Msg(sol_dict['u_control'][1]))
+					self.acc_prev   = sol_dict['u_control'][0]
+					self.steer_prev = sol_dict['u_control'][1]
+
+				if 'warm_start' not in update_dict.keys():
+					update_dict['warm_start'] = {}
+					
+				update_dict['warm_start']['z_ws']  = sol_dict['z_mpc'] 
+				update_dict['warm_start']['u_ws']  = sol_dict['u_mpc'] 
+				update_dict['warm_start']['sl_ws'] = sol_dict['sl_mpc'] 
+
+				self.publish_mpc_path_message(sol_dict)
+
+			loop_rate.sleep()
+
+	def publish_mpc_path_message(self, sol_dict):
+		mpc_path_msg = mpc_path()
+		
+		mpc_path_msg.header.stamp = rospy.get_rostime()
+		mpc_path_msg.solve_status = 'optimal' if sol_dict['optimal'] else 'suboptimal'
+		mpc_path_msg.solve_time = sol_dict['solve_time']
+
+		mpc_path_msg.xs   = sol_dict['z_mpc'][:,0] # x_mpc
+		mpc_path_msg.ys   = sol_dict['z_mpc'][:,1] # y_mpc
+		mpc_path_msg.psis = sol_dict['z_mpc'][:,2] # psi_mpc
+		mpc_path_msg.vs   = sol_dict['z_mpc'][:,3] # v_mpc
+
+		mpc_path_msg.xr   = sol_dict['z_ref'][:,0] # x_ref
+		mpc_path_msg.yr   = sol_dict['z_ref'][:,1] # y_ref
+		mpc_path_msg.psir = sol_dict['z_ref'][:,2] # psi_ref
+		mpc_path_msg.vr   = sol_dict['z_ref'][:,3] # v_ref
+
+		mpc_path_msg.acc  = sol_dict['u_mpc'][:,0] # acc_mpc
+		mpc_path_msg.df   = sol_dict['u_mpc'][:,1] # df_mpc
+
+		self.mpc_path_pub.publish(mpc_path_msg)
+				
+###########################################
+#### Main Function
+###########################################
+if __name__=='__main__':	
+	MPCCommandPublisher()
